c70bf6dee659d8e242dde4521b5dd19823ff1327
peihsuanlin5566 <jnes40722@gmail.com>
2022-10-30 15:47:15 +0900

    equation1 done

--- practice/model/recommend.py
+++ practice/model/recommend.py
@@ -14,15 +14,25 @@ class Recommend:
     def recommend(self, user_id): 
         ...
     
-    def equation_1(self, user_id, ):
+    def equation_1(self, user_id ):
         
         user_ratings = self.ratings.get_ratings(user_id)
         it = len(user_ratings)
         movie_ids =  [ user_rating.movieId for user_rating in user_ratings ]
         rt = { }
         for movie_id in movie_ids: 
-            tag = self.genome_scores.get_tags_by_movieid(movie_id)
-            rt[tag] = self.ratings.get_ratings_by_movies(movie_id, user_id)
+            tags = self.genome_scores.get_tags_by_movieid(movie_id)
+
+            for tag_id in tags:
+                if tag_id not in rt:
+                    rt[tag_id] = []
+                rt[tag_id].append(self.ratings.get_ratings_by_movies(movie_id, user_id))
+            
+        
+        for tag_id in rt.keys(): 
+            rt[tag_id] = sum(rt[tag_id])/it
 
 
-        return 
+        return rt
+    
+    # def  equation2(): 
