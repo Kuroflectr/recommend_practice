81ecf6dc1691b74e54a02f838bd163db9acc3002
peihsuanlin5566 <jnes40722@gmail.com>
2022-11-06 14:16:45 +0900

    evaluate by user

--- practice/model/user_based_recommend.py
+++ practice/model/user_based_recommend.py
@@ -46,12 +46,11 @@ class UserBasedRecommend( Recommend ):
         return recommend_rating_list
 
     def recommend(self, user_id): 
-        # find the movie-id that was not rated by any user; we will predict these values: 
-        self.ratings.set_unrated_movie_id() 
-
         # extract the necessary movie rating column from rating matrix
         ratings_matrix = self.ratings.get_ratings_matrix()
 
+        # find the movie-id that was not rated by any user; we will predict these values: 
+        self.ratings.set_unrated_movie_id() 
         # fetch the ids of the unrated movie by the specified user id
         unrated_movieid_list_user = np.array(self.ratings.unrated_movieid_list[user_id])
 
@@ -65,20 +64,50 @@ class UserBasedRecommend( Recommend ):
 
         return cosine
 
-    def evaluate(self): 
-        ...
-
-    def evaluate_by_user(self, uer_id):
-
-        ...
-        # rated_movie_id
-
-        # evaluated_value = DCG
+    # def evaluate(self): 
         
-        # user_list = self.ratings.user_list
-        # return evaluated_value
+
+
+    #     return
+
+    def evaluate_by_user(self, user_id):
+        self.ratings.set_rating_by_user()
+
+        # extract the necessary movie rating column from rating matrix
+        ratings_matrix = self.ratings.get_ratings_matrix()
+
+        # fetch the ids of the rated movie by the specified user id
+        rated_movieid_list_user = np.array([x.movieId for x in self.ratings.rating_by_user[user_id]])
+
+        # predict the ratings corresponding to the movied id included in "unrated_movieid_list_user" 
+        recommend_rating_list = self.recommend_rating(user_id, ratings_matrix, rated_movieid_list_user)
+
+        # find the rank of the recommendation: 
+        ranks_predict = self.find_order(recommend_rating_list)
+
+
+        # calculate the DCG_ture by the specified user
+        rated_ratings_list_user = np.array([x.rating for x in self.ratings.rating_by_user[user_id]])
+
+        # calculate the DCG_predic by the specified user
+        DCG_predict = self.DCG(ranks_predict, rated_ratings_list_user)
+
+        ranks_true = self.find_order(rated_ratings_list_user)
+        DCG_true = self.DCG(ranks_true, rated_ratings_list_user )
+
+        DCG_value_byuser = DCG_predict.sum()/DCG_true.sum()
+
+        return DCG_value_byuser
+
+    def find_order(self, array: np.array): 
+        temp = np.argsort(-array)
+        ranks = np.empty_like(temp)
+        ranks[temp] = np.arange(len(array))
+            
+        return ranks+1
+
     
-    def DCG(self, order, rating):
+    def DCG(self, order: np.array, rating: np.array) -> np.array:
         
         return (2**(rating)-1)/(np.log2(order+1)) 
     
